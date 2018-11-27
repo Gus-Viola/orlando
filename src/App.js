217636d84c2534aaa50c77b449157687a46a5a24
Gus Viola <gus.viola@pobox.com>
2018-11-27 07:49:26 -0200

    Adjust overall layout of header and body

--- src/App.js
+++ src/App.js
@@ -8,18 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <body>
+        This is a test
+        </body>
       </div>
     );
   }
@@ -28,3 +20,16 @@ class App extends Component {
 //First Commit
 
 export default App;
+
+
+// <p>
+//   Edit <code>src/App.js</code> and save to reload.
+// </p>
+// <a
+//   className="App-link"
+//   href="https://reactjs.org"
+//   target="_blank"
+//   rel="noopener noreferrer"
+// >
+//   Learn React
+// </a>
